515778b140e0130fcc2164fa1a9aa974762de0f5
Al-Amin49 <alamin496832@gmail.com>
2021-03-13 12:24:36 +0600

    learned if-else and-or operator and datetimezone

---
+++ conditional.js
@@ -0,0 +1,26 @@
+var biriyaniPrice = 250;
+if(biriyaniPrice!=250){
+    console.log('I will eat biriyani'); 
+
+}
+else{ 
+console.log('Burger is better');
+}
+
+//multiple condition
+var jobPaiso= true;
+var savingAmount =5000;
+if(jobPaiso == true && savingAmount>200000){
+    console.log('Aso tomar patri kuji '); 
+
+}
+else if(jobPaiso==true){ 
+    console.log('achsa koidin pore dekbo');
+}
+else{ 
+    console.log('Tor kopale biya nai');
+
+}
+// JS date timezone
+var date = new Date();
+console.log(date);
