feba6e57362408312e60a5b2abc9f25f5f95e21d
Al-Amin49 <alamin496832@gmail.com>
2021-03-18 10:54:56 +0600

    module 13 , have learned for and while loop

---
+++ loop.js
@@ -0,0 +1,13 @@
+//while loop
+var num =0;
+while(num<15){
+    console.log('while loop',num);
+    num++;
+}
+//for loop
+var nums= [55,66,77,88,99];
+for(var i=0; i<nums.length;i++){ 
+    var element= nums[i];
+    console.log('for loop',element);
+
+}
