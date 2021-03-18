55f827127492f1c77646c881bfa523f4f77a7f53
Al-Amin49 <alamin496832@gmail.com>
2021-03-18 11:26:16 +0600

    function, parameter, multiple parameter

---
+++ function.js
@@ -0,0 +1,25 @@
+function sayLoveYou(){ 
+    console.log('jaan tumi koi');
+}
+sayLoveYou();
+var date =14;
+var place='Restaurent';
+sayLoveYou();
+
+// function paremeter
+function doubleIt(num){
+    var result = num*2;
+ return result;
+}
+var first = doubleIt(5);
+var second = doubleIt(900);
+var total = first + second;
+console.log(total);
+
+// Multiple parameter
+function add ( num1, num2){ 
+    var result2 = num1+ num2;
+    return result2;
+}
+var sum = add(15,17);
+console.log(sum);
