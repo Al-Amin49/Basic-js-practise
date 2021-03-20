89a08b57252153871526cf47f45ba848fdba4b0a
Al-Amin49 <alamin496832@gmail.com>
2021-03-21 01:30:19 +0600

    learned random number

---
+++ randomNumber.js
@@ -0,0 +1,14 @@
+var num= 2.70;
+console.log(Math.floor(num));
+
+var num2= 2.70;
+console.log(Math.ceil(num2));
+
+var num3= 2.499995;
+console.log(Math.round(num3));
+for(var i=0;i<10;i++){   
+var num4= 2.70;
+var dice = Math.random()*10;
+var randomNum = Math.round(dice);
+console.log('random number =',randomNum);
+}
